d9caff1254ef31c1f367520470bc37a7a1da66fb
Peo0802 <whatthefack000@gmail.com>
2025-01-03 15:55:07 +0700

    Update type.js

--- src/config/type.js
+++ src/config/type.js
@@ -1,6 +1,6 @@
 const status = "AUTOMATIC";
-const botName = "💮 Distube Bot [ Free Tier ] ";
-const text = "GlaceYT";
+const botName = "Quan paylak ";
+const text = "Mama Peo";
 const version = "Latest@ v1.0";
 const startTime = Date.now();
 
